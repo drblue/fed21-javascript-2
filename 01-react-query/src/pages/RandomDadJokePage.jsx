1873b6b644067d299cb94af55ebdffa17ef6e9d4
Johan Nordström <johan@digitalvillage.se>
2022-06-08 11:46:34 +0200

    Our first React Query request.

--- 01-react-query/src/pages/RandomDadJokePage.jsx
+++ 01-react-query/src/pages/RandomDadJokePage.jsx
@@ -1,10 +1,24 @@
 import Container from 'react-bootstrap/Container'
+import { useQuery } from 'react-query'
 import { getRandomDadJoke } from '../services/ICanHazDadJokeAPI'
 
 const RandomDadJokePage = () => {
+	const { isLoading, isError, error, data } = useQuery('random-dad-joke', getRandomDadJoke)
+
 	return (
 		<Container className="py-3">
-			RandomDadJokePage
+
+			<h1>Random Dad Joke</h1>
+
+			{isLoading && (<p>Loading dad joke...</p>)}
+
+			{isError && (<p>An error occurred: {error.message}</p>)}
+
+			{data && (
+				<div className="h3 text-center my-5">
+					{data.joke}
+				</div>
+			)}
 		</Container>
 	)
 }
