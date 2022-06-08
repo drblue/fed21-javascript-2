1fdd7e6a43ba814839efe5ec5a4786004825a703
Johan Nordström <johan@digitalvillage.se>
2022-06-08 10:53:10 +0200

    Installed React Query and added skeleton for Random Dad Joke Page.

--- 01-react-query/src/App.jsx
+++ 01-react-query/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import Navigation from './components/Navigation'
 import HomePage from './pages/HomePage'
 import PageNotFound from './pages/PageNotFound'
+import RandomDadJokePage from './pages/RandomDadJokePage'
 import './assets/scss/App.scss'
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
 
 			<Routes>
 				<Route path="/" element={<HomePage />} />
+				<Route path="/random-dad-joke" element={<RandomDadJokePage />} />
 				<Route path="*" element={<PageNotFound />} />
 			</Routes>
 		</div>
---
+++ 01-react-query/src/pages/RandomDadJokePage.jsx
@@ -0,0 +1,11 @@
+import Container from 'react-bootstrap/Container'
+
+const RandomDadJokePage = () => {
+	return (
+		<Container className="py-3">
+			RandomDadJokePage
+		</Container>
+	)
+}
+
+export default RandomDadJokePage
