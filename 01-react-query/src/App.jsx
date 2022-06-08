3760a1acfc8c6486d5d95d4eb19fa039aba37071
Johan Nordström <johan@digitalvillage.se>
2022-06-08 10:40:48 +0200

    Configured React Router, added HomePage etc.

--- 01-react-query/src/App.jsx
+++ 01-react-query/src/App.jsx
@@ -1,8 +1,19 @@
+import { Routes, Route } from 'react-router-dom'
+import Navigation from './components/Navigation'
+import HomePage from './pages/HomePage'
+import PageNotFound from './pages/PageNotFound'
 import './assets/scss/App.scss'
 
 const App = () => {
 	return (
-		<div>App</div>
+		<div id="App">
+			<Navigation />
+
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="*" element={<PageNotFound />} />
+			</Routes>
+		</div>
 	)
 }
 
