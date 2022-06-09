27c9c546208b8c074afc75fe4097ceafbb684d23
Johan Nordström <johan@digitalvillage.se>
2022-06-09 10:03:46 +0200

    Added Dad Jokes API

---
+++ 01-react-query/src/pages/RandomDadJokePage.jsx
@@ -0,0 +1,27 @@
+import Container from 'react-bootstrap/Container'
+import { useQuery } from 'react-query'
+import { getRandomJoke } from '../services/DadJokesAPI'
+
+const RandomDadJokePage = () => {
+	const { isLoading, isError, error, data } = useQuery('random-dad-joke', getRandomJoke)
+
+	return (
+		<Container className="py-3">
+
+			<h1>Random Dad Joke</h1>
+
+			{isLoading && (<p>Loading dad joke...</p>)}
+
+			{isError && (<p>An error occurred: {error.message}</p>)}
+
+			{data && data.body.map(joke => (
+				<div key={joke._id} className="text-center my-5">
+					<p className="h3">{joke.setup}</p>
+					<p className="h4">{joke.punchline}</p>
+				</div>
+			))}
+		</Container>
+	)
+}
+
+export default RandomDadJokePage
