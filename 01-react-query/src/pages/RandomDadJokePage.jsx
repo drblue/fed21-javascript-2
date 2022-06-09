e754c800d566205f32d8584a4f822ad160fdef69
Johan Nordström <johan@digitalvillage.se>
2022-06-09 13:36:58 +0200

    Randomize button text for dad jokes.

--- 01-react-query/src/pages/RandomDadJokePage.jsx
+++ 01-react-query/src/pages/RandomDadJokePage.jsx
@@ -1,9 +1,27 @@
+import { useEffect, useState } from 'react'
+import Button from 'react-bootstrap/Button'
 import Container from 'react-bootstrap/Container'
 import { useQuery } from 'react-query'
 import { getRandomJoke } from '../services/DadJokesAPI'
 
+const buttonTexts = [
+	"Ugh.",
+	"🤦🏻‍♂️",
+	"omg dad.",
+	"you are the worst",
+	"seriously",
+	"stop it.",
+	"please stop",
+	"that was the worst one",
+]
+
 const RandomDadJokePage = () => {
-	const { isLoading, isError, error, data } = useQuery('random-joke', getRandomJoke)
+	const [buttonText, setButtonText] = useState()
+	const { isLoading, isError, error, data, refetch } = useQuery('random-joke', getRandomJoke)
+
+	useEffect(() => {
+		setButtonText(buttonTexts[Math.floor(Math.random() * buttonTexts.length)])
+	}, [data])
 
 	return (
 		<Container className="py-3">
@@ -20,6 +38,12 @@ const RandomDadJokePage = () => {
 					<p className="h4">{joke.punchline}</p>
 				</div>
 			))}
+
+			<div className="d-flex justify-content-center">
+				<Button variant="primary" onClick={refetch}>
+					{buttonText ?? 'Moar!'}
+				</Button>
+			</div>
 		</Container>
 	)
 }
