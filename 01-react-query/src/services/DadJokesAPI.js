71f1b3f8b319f9d005d0c7f86f4f5e062d118699
Johan Nordström <johan@digitalvillage.se>
2022-06-09 10:24:27 +0200

    Fixed my mind tripping me up.

--- 01-react-query/src/services/DadJokesAPI.js
+++ 01-react-query/src/services/DadJokesAPI.js
@@ -1,13 +1,16 @@
 import axios from 'axios'
 
 axios.defaults.baseURL = 'https://dad-jokes.p.rapidapi.com'
-axios.defaults.headers = {
-	'X-RapidAPI-Key': import.meta.env.VITE_DAD_JOKES_API_KEY,
-	'X-RapidAPI-Host': 'dad-jokes.p.rapidapi.com',
+
+const requestOptions = {
+	headers: {
+		'X-RapidAPI-Key': import.meta.env.VITE_DAD_JOKES_API_KEY,
+		'X-RapidAPI-Host': 'dad-jokes.p.rapidapi.com',
+	}
 }
 
 export const getRandomJoke = async () => {
-	const response = await axios.get('/random/joke')
+	const response = await axios.get('/random/joke', requestOptions)
 	return response.data
 }
 
