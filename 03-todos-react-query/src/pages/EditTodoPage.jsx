6a2b83a1255a0c75d81c1d975433d43bae97a4df
Johan Nordström <johan@digitalvillage.se>
2022-06-14 22:32:17 +0200

    Added useQuery to EditTodoPage. And commented out the PUT/DELETE requests.

--- 03-todos-react-query/src/pages/EditTodoPage.jsx
+++ 03-todos-react-query/src/pages/EditTodoPage.jsx
@@ -1,36 +1,36 @@
 import { useState, useEffect } from 'react'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
+import { useQuery } from 'react-query'
 import { useNavigate, useParams } from 'react-router-dom'
+import WarningAlert from '../components/alerts/WarningAlert'
+import LoadingSpinner from '../components/LoadingSpinner'
 import TodosAPI from '../services/TodosAPI'
 
 const EditTodoPage = () => {
-	const [todo, setTodo] = useState()
 	const [newTitle, setNewTitle] = useState("")
 	const { id } = useParams()
 	const navigate = useNavigate()
-
-	const getTodo = async (id) => {
-		const data = await TodosAPI.getTodo(id)
-		setTodo(data)
-		setNewTitle(data.title)
-	}
+	const { data, error, isError, isLoading } = useQuery(['todo', { id }], () => TodosAPI.getTodo(id))
 
 	const handleDelete = async () => {
 		if (!window.confirm('U SURE BRO?!')) {
 			return
 		}
 
+		/*
 		// send request to API to delete the todo
 		await TodosAPI.deleteTodo(id)
 
 		// navigate user to `/todos`
 		navigate('/todos')
+		*/
 	}
 
 	const handleSubmit = async e => {
 		e.preventDefault()
 
+		/*
 		// send request to API to update title for this todo with the value in the input field
 		await TodosAPI.updateTodo(id, {
 			title: newTitle,
@@ -38,38 +38,44 @@ const EditTodoPage = () => {
 
 		// redirect user to /todos/:id
 		navigate(`/todos/${id}`)
+		*/
 	}
 
 	useEffect(() => {
-		getTodo(id)
-	}, [id])
-
-	if (!todo) {
-		return <p>Loading...</p>
-	}
+		if (data) {
+			setNewTitle(data.title)
+		}
+	}, [data])
 
 	return (
 		<div>
-			<h1>Edit: {todo.title}</h1>
+			{isLoading && <LoadingSpinner />}
 
-			<Form onSubmit={handleSubmit}>
-				<Form.Group className="mb-3" controlId="newTitle">
-					<Form.Label>Title</Form.Label>
-					<Form.Control
-						onChange={e => setNewTitle(e.target.value)}
-						placeholder="Enter title"
-						required
-						type="text"
-						value={newTitle}
-					/>
-				</Form.Group>
+			{isError && <WarningAlert error={error.message} />}
 
-				<div className="d-flex justify-content-between">
-					<Button variant="success" type="submit" disabled={!newTitle.length}>Save</Button>
-					<Button variant="danger" onClick={handleDelete}>Delete</Button>
-				</div>
-			</Form>
+			{data && (
+				<>
+					<h1>Edit: {data.title}</h1>
 
+					<Form onSubmit={handleSubmit}>
+						<Form.Group className="mb-3" controlId="newTitle">
+							<Form.Label>Title</Form.Label>
+							<Form.Control
+								onChange={e => setNewTitle(e.target.value)}
+								placeholder="Enter title"
+								required
+								type="text"
+								value={newTitle}
+							/>
+						</Form.Group>
+
+						<div className="d-flex justify-content-between">
+							<Button variant="success" type="submit" disabled={!newTitle.length}>Save</Button>
+							<Button variant="danger" onClick={handleDelete}>Delete</Button>
+						</div>
+					</Form>
+				</>
+			)}
 		</div>
 	)
 }
