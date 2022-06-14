1b15343ed61034b257e84e93d7d70de9c5d5835c
Johan Nordström <johan@digitalvillage.se>
2022-06-14 22:28:36 +0200

    Added SASS, React Spinners and React Query.

--- 03-todos-react-query/src/App.jsx
+++ 03-todos-react-query/src/App.jsx
@@ -1,4 +1,5 @@
 import Container from 'react-bootstrap/Container'
+import { ReactQueryDevtools } from 'react-query/devtools'
 import { Routes, Route } from 'react-router-dom'
 import Navigation from './components/Navigation'
 import CreateTodoPage from './pages/CreateTodoPage'
@@ -8,7 +9,7 @@ import NotFound from './pages/NotFound'
 import TodosPage from './pages/TodosPage'
 import TodoPage from './pages/TodoPage'
 import 'bootstrap/dist/css/bootstrap.css'
-import './App.css'
+import './assets/scss/App.scss'
 
 const App = () => {
 
@@ -26,6 +27,8 @@ const App = () => {
 					<Route path="*" element={<NotFound />} />
 				</Routes>
 			</Container>
+
+			<ReactQueryDevtools position='bottom-right' />
 		</div>
 	)
 }
--- 03-todos-react-query/src/index.js
+++ 03-todos-react-query/src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from 'react-query'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App';
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			staleTime: 60 * 1000, // 1 minute
+			cacheTime: 5 * 60 * 1000, // 5 minutes
+		}
+	}
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
+		<QueryClientProvider client={queryClient}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</QueryClientProvider>
 	</React.StrictMode>
 );
