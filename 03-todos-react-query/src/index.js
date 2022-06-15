608eb8a675b323022e288bcefe783b23f7d1a425
Johan Nordström <johan@digitalvillage.se>
2022-06-15 09:20:37 +0200

    Don't refetch data on window/tab focus plz.

--- 03-todos-react-query/src/index.js
+++ 03-todos-react-query/src/index.js
@@ -7,6 +7,7 @@ import App from './App';
 const queryClient = new QueryClient({
 	defaultOptions: {
 		queries: {
+			refetchOnWindowFocus: false,
 			staleTime: 60 * 1000, // 1 minute
 			cacheTime: 5 * 60 * 1000, // 5 minutes
 		}
