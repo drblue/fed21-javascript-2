fc239fa7dc1a4d764316221cfe284bedc3d8c45a
Johan Nordström <johan@digitalvillage.se>
2022-06-15 09:59:52 +0200

    Moved edit todo form to its own component.

--- 03-todos-react-query/src/pages/EditTodoPage.jsx
+++ 03-todos-react-query/src/pages/EditTodoPage.jsx
@@ -1,23 +1,16 @@
-import { useState, useEffect } from 'react'
-import Button from 'react-bootstrap/Button'
-import Form from 'react-bootstrap/Form'
 import { useQuery } from 'react-query'
 import { useNavigate, useParams } from 'react-router-dom'
 import WarningAlert from '../components/alerts/WarningAlert'
+import EditTodoForm from '../components/EditTodoForm'
 import LoadingSpinner from '../components/LoadingSpinner'
 import TodosAPI from '../services/TodosAPI'
 
 const EditTodoPage = () => {
-	const [newTitle, setNewTitle] = useState("")
 	const { id } = useParams()
 	const navigate = useNavigate()
 	const { data, error, isError, isLoading } = useQuery(['todo', { id }], () => TodosAPI.getTodo(id))
 
 	const handleDelete = async () => {
-		if (!window.confirm('U SURE BRO?!')) {
-			return
-		}
-
 		/*
 		// send request to API to delete the todo
 		await TodosAPI.deleteTodo(id)
@@ -27,9 +20,7 @@ const EditTodoPage = () => {
 		*/
 	}
 
-	const handleSubmit = async e => {
-		e.preventDefault()
-
+	const handleSubmit = async (data) => {
 		/*
 		// send request to API to update title for this todo with the value in the input field
 		await TodosAPI.updateTodo(id, {
@@ -41,12 +32,6 @@ const EditTodoPage = () => {
 		*/
 	}
 
-	useEffect(() => {
-		if (data) {
-			setNewTitle(data.title)
-		}
-	}, [data])
-
 	return (
 		<div>
 			{isLoading && <LoadingSpinner />}
@@ -57,23 +42,7 @@ const EditTodoPage = () => {
 				<>
 					<h1>Edit: {data.title}</h1>
 
-					<Form onSubmit={handleSubmit}>
-						<Form.Group className="mb-3" controlId="newTitle">
-							<Form.Label>Title</Form.Label>
-							<Form.Control
-								onChange={e => setNewTitle(e.target.value)}
-								placeholder="Enter title"
-								required
-								type="text"
-								value={newTitle}
-							/>
-						</Form.Group>
-
-						<div className="d-flex justify-content-between">
-							<Button variant="success" type="submit" disabled={!newTitle.length}>Save</Button>
-							<Button variant="danger" onClick={handleDelete}>Delete</Button>
-						</div>
-					</Form>
+					<EditTodoForm todo={data} onDelete={handleDelete} onSubmit={handleSubmit} />
 				</>
 			)}
 		</div>
