16ccf4539b5dc6d2cdc941acec367e184d234a2a
Johan Nordström <johan@digitalvillage.se>
2022-06-15 12:01:13 +0200

    Toggle a todo's status.

--- 03-todos-react-query/src/pages/TodoPage.jsx
+++ 03-todos-react-query/src/pages/TodoPage.jsx
@@ -1,5 +1,5 @@
 import Button from 'react-bootstrap/Button'
-import { useQuery } from 'react-query'
+import { useQuery, useMutation, useQueryClient } from 'react-query'
 import { Link, useParams } from 'react-router-dom'
 import WarningAlert from '../components/alerts/WarningAlert'
 import LoadingSpinner from '../components/LoadingSpinner'
@@ -7,16 +7,21 @@ import TodosAPI from '../services/TodosAPI'
 
 const TodoPage = () => {
 	const { id } = useParams()
+	const queryClient = useQueryClient()
 	const { data, error, isError, isLoading } = useQuery(['todo', { id }], () => TodosAPI.getTodo(id))
+
+	const toggleTodoMutation = useMutation(() => {
+		return TodosAPI.updateTodo(id, {
+			completed: !data.completed
+		})
+	})
 
 	// Toggle the completed status of a todo in the api
 	const toggleTodo = async () => {
-		/*
-		await TodosAPI.updateTodo(id, {
-			completed: !data.completed
-		})
-		getTodo(id)
-		*/
+		await toggleTodoMutation.mutateAsync()
+
+		// invalidate todo query
+		queryClient.invalidateQueries(['todo', { id }])
 	}
 
 	return (
@@ -31,7 +36,7 @@ const TodoPage = () => {
 
 					<p><strong>Status:</strong> {data.completed ? 'Completed' : 'Not completed'}</p>
 
-					<Button variant="success" onClick={toggleTodo}>Toggle</Button>
+					<Button variant="success" onClick={toggleTodo} disabled={toggleTodoMutation.isLoading}>Toggle</Button>
 					<Button variant="warning" as={Link} to={`/todos/${id}/edit`}>Edit</Button>
 				</>
 			)}
