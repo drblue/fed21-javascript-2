2db40a907d62fe0f95e226460a1efb3dffdc16b4
Johan Nordström <johan@digitalvillage.se>
2022-06-17 11:54:12 +0200

    I love dem animations.

--- 03-todos-react-query/src/components/animations/PageTransition.jsx
+++ 03-todos-react-query/src/components/animations/PageTransition.jsx
@@ -1,19 +1,73 @@
 import { motion } from 'framer-motion'
 
 const transition = {
-	// duration: 2,
+	// duration: 0.5,
+	// ease: "easeInOut",
+	type: "spring",
+	mass: 0.4,
+	damping: 8,
 }
 
 const fadeIn = {
 	initial: { opacity: 0 },
 	enter: { opacity: 1, transition },
-	exit: { opacity: 0 },
+	exit: { opacity: 0, transition }
+}
+
+const swipeInFromLeftAndFade = {
+	initial: { x: -100, opacity: 0 },
+	enter: { x: 0, opacity: 1, transition },
+	exit: { x: 100, opacity: 0, transition }
+}
+
+const swipeInFromRightAndFade = {
+	initial: { x: 100, opacity: 0 },
+	enter: { x: 0, opacity: 1, transition },
+	exit: { x: -100, opacity: 0, transition }
+}
+
+const swipeInFromTopAndFade = {
+	initial: { y: -100, opacity: 0 },
+	enter: { y: 0, opacity: 1, transition },
+	exit: { y: 100, opacity: 0, transition }
+}
+
+const swipeInFromBottomAndFade = {
+	initial: { y: 100, opacity: 0 },
+	enter: { y: 0, opacity: 1, transition },
+	exit: { y: -100, opacity: 0, transition }
+}
+
+const spring = {
+	initial: {
+		opacity: 0,
+		x: "-50vw",
+		transition: {
+			// staggerChildren: 0.5,
+		}
+	},
+	enter: {
+		opacity: 1,
+		x: 0,
+		transition: {
+			...transition,
+		}
+	},
+	exit: {
+		opacity: 0,
+		x: "-50vw",
+		transition: {
+			...transition,
+			mass: 0.01,
+			// staggerChildren: 0.5,
+		}
+	},
 }
 
 const PageTransition = ({ children }) => {
 	return (
 		<motion.div
-			variants={fadeIn}
+			variants={spring}
 			initial="initial"
 			animate="enter"
 			exit="exit"
